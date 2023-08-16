a04e9679dfdff865a900944499f442083bbb1f02
Javier Grau <javiergrau.freelance@gmail.com>
2023-08-16 17:50:24 -0400

    UPDATE 'schema.sql', 'queries.sql' and 'data.sql'

--- data.sql
+++ data.sql
@@ -5,13 +5,63 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Agumon', '2020-02-03', 0, true, 10.23),
        ('Gabumon', '2018-11-15', 2, true, 8),
        ('Pikachu', '2021-01-7', 1, false, 15.04),
-       ('Devimon', '2017-05-12', 5, false, 11),
+       ('Devimon', '2017-05-12', 5, false, 11);
 
 /* MILESTONE 2 - Query and update animals table */
-       ('Charmander', '2020-02-08', 0, false, -11),
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Charmander', '2020-02-08', 0, false, -11),
        ('Plantmon', '2021-11-15', 2, true, -5.7),
        ('Squirtle', '1993-04-02', 3, false, -12.13),
        ('Angemon', '2005-06-12', 1, true, -45),
        ('Boarmon', '2005-06-07', 7, true, 20.4),
        ('Blossom', '1998-10-13', 3, true, 17),
        ('Ditto', '2022-05-14', 4, true, 22);
+
+/* MILESTONE 3 - Query multiple tables */
+INSERT INTO owners (full_name, age)
+       VALUES ('Sam Smith', 34),
+              ('Jennifer Orwell', 19),
+              ('Bob', 45),
+              ('Melody Pond', 77),
+              ('Dean Winchester', 14),
+              ('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+       VALUES ('Pokemon'),
+              ('Digimon');
+
+BEGIN;
+UPDATE animals
+    SET species_id = 2
+    WHERE name LIKE '%___mon';
+UPDATE animals
+    SET species_id = 1
+    WHERE species_id IS NULL;
+COMMIT;
+
+BEGIN;
+UPDATE animals
+    SET owners_id = 1
+    WHERE animals.name = 'Agumon';
+SAVEPOINT sp1;
+UPDATE animals
+    SET owners_id = 2
+    WHERE animals.name = 'Gabumon'
+	OR animals.name = 'Pikachu';
+SAVEPOINT sp2;
+UPDATE animals
+    SET owners_id = 3
+    WHERE animals.name = 'Devimon'
+	OR animals.name = 'Plantmon'
+SAVEPOINT sp3;
+UPDATE animals
+    SET owners_id = 4
+    WHERE animals.name = 'Charmander'
+	OR animals.name = 'Squirtle'
+	OR animals.name = 'Blossom';
+SAVEPOINT sp4;
+UPDATE animals
+    SET owners_id = 5
+    WHERE animals.name = 'Angemon'
+	OR animals.name = 'Boarmon';
+COMMIT;
--- queries.sql
+++ queries.sql
@@ -41,32 +41,95 @@ UPDATE animals
     WHERE weight_kg < 0;
 COMMIT;
 
-/* How many animals are there? */
+-- How many animals are there?
 SELECT COUNT(id) AS animal_count FROM animals;
 
-/* How many animals have never tried to escape? */
-SELECT COUNT(id) AS neutered_count FROM animals
-    WHERE neutered = false;
+-- How many animals have never tried to escape?
+SELECT COUNT(animals.id) AS amount_of_animals_never_escaped
+    FROM animals
+    WHERE escape_attempts = 0;
 
-/* What is the average weight of animals? */
+-- What is the average weight of animals?
 SELECT AVG(weight_kg) AS avg_weigth_kg FROM animals;
 
-/* Who escapes the most, neutered or not neutered animals? */
+-- Who escapes the most, neutered or not neutered animals?
 SELECT neutered, MAX(escape_attempts) AS max_esc_attempts
     FROM animals
     GROUP BY neutered
     ORDER BY max_esc_attempts DESC;
 
-/* What is the minimum and maximum weight of each type of animal? */
+-- What is the minimum and maximum weight of each type of animal?
 SELECT species,
     MIN(weight_kg) AS min_weight,
     MAX(weight_kg) AS max_weight
     FROM animals
     GROUP BY species;
 
-/* What is the average number of escape attempts per animal type of those born between 1990 and 2000? */
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT species,
     AVG(escape_attempts) AS avg_esc_attempts
     FROM animals
     WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01'
     GROUP BY species;
+
+/* MILESTONE 3 - Query multiple tables */
+-- What animals belong to Melody Pond?
+SELECT owners.full_name AS owner, 
+	STRING_AGG(animals.name, ', ') AS pokemons
+    FROM owners
+    JOIN animals
+    ON owners_id = owners.id
+    WHERE full_name = 'Melody Pond'
+	GROUP BY owner;
+
+--List of all animals that are pokemon (their type is Pokemon).
+SELECT animals.name AS pokemons
+    FROM animals
+    JOIN species
+    ON species_id = species.id
+    WHERE species.name = 'Pokemon';
+
+-- List all owners and their animals, remember to include those that don't own any animal.
+SELECT full_name AS owner, 
+    STRING_AGG(animals.name, ', ') AS pokemons
+    FROM owners
+    LEFT JOIN animals
+    ON owners.id = owners_id
+    GROUP BY owners.full_name;
+
+-- How many animals are there per species?
+SELECT species.name AS animal_type,
+    COUNT(animals.species_id) AS amount
+    FROM species
+    JOIN animals
+    ON species.id = species_id
+    GROUP BY animal_type;
+
+-- List all Digimon owned by Jennifer Orwell.
+SELECT owners.full_name AS owner,
+    animals.name AS digimon
+    FROM owners
+    JOIN animals
+    ON owners.id = owners_id
+    JOIN species
+    ON species.id = species_id
+    WHERE owners.full_name = 'Jennifer Orwell'
+    AND species.name = 'Digimon';
+
+-- List all animals owned by Dean Winchester that haven't tried to escape.
+SELECT animals.name AS Mr_Winchester_pets,
+    animals.escape_attempts
+    FROM animals
+    JOIN owners
+    ON owners_id = owners.id
+    WHERE owners.full_name = 'Dean Winchester'
+    AND animals.escape_attempts = 0;
+
+-- Who owns the most animals?
+SELECT owners.full_name AS owners,
+    COUNT(animals.owners_id) AS pets_amount
+    FROM owners
+    LEFT JOIN animals
+    ON owners.id = owners_id
+    GROUP BY owners
+    ORDER BY pets_amount DESC;
