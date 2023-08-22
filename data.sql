022a7cf633e67f36fa156e7dfd88c74230a037e1
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-08-22 17:45:12 +0300

    Add content into visits and owners columns

--- data.sql
+++ data.sql
@@ -114,3 +114,10 @@ SELECT
 	)
 		AND vets.id = visits.vets_id
 	GROUP BY vets.name, species.name;
+
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, visit_date) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
